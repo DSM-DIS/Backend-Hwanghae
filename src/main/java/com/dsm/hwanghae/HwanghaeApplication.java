232f40c535402bbc42130f48d8645334efe5fd59
Lee-Jin-Hyeok <jinjin0816@dsm.hs.kr>
2020-09-13 19:59:29 +0900

    [CREATE] Start Project

---
+++ src/main/java/com/dsm/hwanghae/HwanghaeApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.hwanghae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwanghaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwanghaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsm/hwanghae/HwanghaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsm.hwanghae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwanghaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
