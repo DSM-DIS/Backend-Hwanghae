812ed40affe5a9727a932a898e48460a33de5387
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-04 08:45:12 +0900

    FINALLY

--- src/main/java/com/dsm/hwanghae/controller/TestController.java
+++ src/main/java/com/dsm/hwanghae/controller/TestController.java
@@ -1,8 +1,6 @@
 package com.dsm.hwanghae.controller;
 
 import com.dsm.hwanghae.domains.domain.Student;
-import com.dsm.hwanghae.domains.service.DiaryBookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
