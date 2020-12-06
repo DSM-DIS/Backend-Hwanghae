aea6dbe1fffdb21fe1daa9c9d074eab78eda5f4c
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-06 21:58:29 +0900

    FINALLY

--- src/main/java/com/dsm/hwanghae/controller/DiaryBookController.kt
+++ src/main/java/com/dsm/hwanghae/controller/DiaryBookController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 
 @RestController
-class DiaryBookController(@Autowired val diaryBookService: DiaryBookService) {
+class DiaryBookController(private val diaryBookService: DiaryBookService) {
 
     @GetMapping("/user/diary-books")
     fun getDiaryBook(request: HttpServletRequest): DiaryBookResponseForm {
