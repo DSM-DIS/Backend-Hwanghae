da8efe65cec62e90261d0cf08d921d912467cfb7
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-06 20:57:29 +0900

    FINALLY

--- src/main/java/com/dsm/hwanghae/controller/TestController.java
+++ src/main/java/com/dsm/hwanghae/controller/TestController.java
@@ -3,6 +3,7 @@ package com.dsm.hwanghae.controller;
 import com.dsm.hwanghae.domains.domain.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +23,7 @@ public class TestController {
     }
 
     @PostMapping("/testtesttest")
-    public Student postMapping(Student student) {
+    public Student postMapping(@RequestBody Student student) {
         System.out.println("student : " + student.toString());
         student.setName("name change");
         return student;
