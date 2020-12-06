9f7e606cf69066030c20e5ea81d76f2c3d0c65aa
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-06 23:26:36 +0900

    FINALLY

--- src/main/java/com/dsm/hwanghae/controller/DiaryBookController.java
+++ src/main/java/com/dsm/hwanghae/controller/DiaryBookController.java
@@ -1,9 +1,12 @@
 package com.dsm.hwanghae.controller;
 
+import com.dsm.hwanghae.domains.domain.Student;
 import com.dsm.hwanghae.domains.service.DiaryBookService;
 import com.dsm.hwanghae.utils.form.DiaryBookResponseForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,5 +25,16 @@ public class DiaryBookController {
     public DiaryBookResponseForm getDiaryBook(HttpServletRequest request) {
         String userId = request.getHeader("userId");
         return diaryBookService.getDiaryBooks(userId);
+    }
+
+    @GetMapping("/testtesttest")
+    public Student getMapping() {
+        return new Student("2417", "이진혁");
+    }
+
+    @PostMapping("/testtesttest")
+    public Student postMapping(@RequestBody Student student) {
+        student.setName("change name");
+        return student;
     }
 }
