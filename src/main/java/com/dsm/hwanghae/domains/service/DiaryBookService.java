8997ec2add3e906621a39540994b2d037e8355d4
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-12-22 15:25:03 +0900

    <DiaryBookService> print log

--- src/main/java/com/dsm/hwanghae/domains/service/DiaryBookService.java
+++ src/main/java/com/dsm/hwanghae/domains/service/DiaryBookService.java
@@ -28,7 +28,7 @@ public class DiaryBookService {
         Response<DiaryBookResponseForm> response = null;
         try {
             response = service.addRequest(userId).execute();
-            System.out.println("first value : " + response.body());
+            System.out.println("name : " + response.body().getDiaryBooks().get(0).getName());
         } catch(IOException e) {
             e.printStackTrace();
         }
